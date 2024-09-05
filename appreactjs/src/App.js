5c14392d444a56c97ce5a44d27d552a63a19ef85
Luan <lm7007fernandes@gmail.com>
2024-09-05 10:32:30 -0300

    Criação de novos componentes

--- appreactjs/src/App.js
+++ appreactjs/src/App.js
@@ -1,32 +1,26 @@
 
 import './App.css';
 import {Component} from 'react';
+import { loadPosts } from './utils';
 
 class App extends Component {
   state = {
     posts: []
   };
 
-  timeoutUpdate = null;
-
-  componentDidMount()
+  async componentDidMount()
   {
-    this.loadPosts();
+   await this.loadPosts();
   }
 
-  loadPosts= async () => 
-    {
-    const postsResponse =  fetch('https://jsonplaceholder.typicode.com/posts');
-    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos')
-    const [posts, photos] = await Promise.all([postsResponse, photoResponse]);
-    const postsJson = await posts.json();
-    const photoJson = await photos.json();
+ 
 
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return {... post, cover: photoJson[index].url}
-    });
-    this.setState({posts: postsAndPhotos});
-  }
+    loadPosts = async () => {
+      const postsAndPhotos = await loadPosts
+      this.setState({posts: postsAndPhotos});
+    } 
+    
+  
 
   render () {
     const { posts} = this.state;
@@ -34,25 +28,14 @@ class App extends Component {
     
     return (
       <section className='container'>
-        <div className="posts">
-        {posts.map( post => (
-          <div className='post'>
-            <img src={post.cover} alt={post.title} />
-          <div key={post.id} className='post-content'>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          </div>
-          </div>
-          ))}
-      </div>
+       <posts posts = {posts} />
     
       </section>
     );
-  }
-  }
+}
+}
 
 
-  
 
 
 export default App;
